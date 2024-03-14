05dde8c6ae4733b1526c12f4f89de2d5c96cd26a
Kyle Nguyen <newwinn.kyle@gmail.com>
2024-03-14 13:56:55 -0700

    Created test.js for sortPoints.js and added two valid tests

---
+++ src/lib/sortPoints.test.js
@@ -0,0 +1,58 @@
+const sortPoints = require("./sortPoints")
+
+test("sortPoints unit test for valid array of points", function() {
+    const preSort = [
+        { x: 3, y: 5},
+        { x: 1, y: 8},
+        { x: 2, y: 4}
+    ]
+
+    const postSort = [
+        { x: 1, y: 8},
+        { x: 2, y: 4},
+        { x: 3, y: 5}
+    ]
+
+    const sort = sortPoints(preSort)
+    expect(sort).toEqual(postSort)
+})
+
+test("sortPoints unit test for valid array of points", function() {
+    const preSort = [
+        { x: 10, y: 5},
+        { x: 1, y: 8},
+        { x: 2, y: 4},
+        { x: 9, y: 2},
+        { x: 5, y: 3},
+        { x: 3, y: 5}
+    ]
+
+    const postSort = [
+        { x: 1, y: 8},
+        { x: 2, y: 4},
+        { x: 3, y: 5},
+        { x: 5, y: 3},
+        { x: 9, y: 2},
+        { x: 10, y: 5}
+    ]
+
+    const sort = sortPoints(preSort)
+    expect(sort).toEqual(postSort)
+})
+
+// test("sortPoints unit test for valid array of points (with duplicate x values)", function() {
+//     const preSort = [
+//         { x: 3, y: 5},
+//         { x: 1, y: 8},
+//         { x: 2, y: 4}
+//     ]
+
+//     const postSort = [
+//         { x: 1, y: 8},
+//         { x: 2, y: 4},
+//         { x: 3, y: 5}
+//     ]
+
+//     const sort = sortPoints(preSort)
+//     expect(sort).toEqual(postSort)
+// })
