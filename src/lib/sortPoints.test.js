a6985807995286c953e7904d696b9a1e0d7be712
Kyle Nguyen <newwinn.kyle@gmail.com>
2024-03-14 14:19:10 -0700

    Added test for sorting array with duplicate x values

--- src/lib/sortPoints.test.js
+++ src/lib/sortPoints.test.js
@@ -67,3 +67,26 @@ test("sortPoints unit test for valid array of points (duplicate x values)", func
     const sort = sortPoints(preSort)
     expect(sort).toEqual(postSort)
 })
+
+test("sortPoints unit test for valid array of points (duplicate x values)", function() {
+    const preSort = [
+        { x: 10, y: 5},
+        { x: 1, y: 8},
+        { x: 1, y: 4},
+        { x: 9, y: 2},
+        { x: 2, y: 3},
+        { x: 2, y: 5}
+    ]
+
+    const postSort = [
+        { x: 1, y: 8},
+        { x: 1, y: 4},
+        { x: 2, y: 3},
+        { x: 2, y: 5},
+        { x: 9, y: 2},
+        { x: 10, y: 5}
+    ]
+
+    const sort = sortPoints(preSort)
+    expect(sort).toEqual(postSort)
+})
