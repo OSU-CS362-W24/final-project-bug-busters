30a2dd20f2c6170f52a0e13f0729adb18f2dfe8a
Kyle Nguyen <newwinn.kyle@gmail.com>
2024-03-16 18:26:57 -0700

    Added tests for adding multiple charts at once to gallery

--- cypress/e2e/saving-chart-to-gallery.cy.js
+++ cypress/e2e/saving-chart-to-gallery.cy.js
@@ -22,4 +22,37 @@ describe('Saving charts to the gallery tests', () => {
 
     cy.findByText("Cats vs. Dogs").should("exist")
   })
+
+  it("Test that when multiple charts are added at once it is properly saved to the gallery", () => {
+    cy.findByText("Line").click()
+
+    cy.addChartTitleAndLabels("Cats vs. Dogs", "Cats", "Dogs")
+
+    cy.addDataPoint("1", "3", 0)
+    cy.addDataPoint("2", "7", 1)
+    cy.addDataPoint("3", "15", 2)
+    cy.addDataPoint("4", "25", 3)
+    cy.addDataPoint("5", "40", 4)
+
+    cy.findByRole("button", { name: "Generate chart" }).click()
+    cy.findByRole("button", { name: "Save chart" }).click()
+
+    cy.findByRole("button", { name: "Clear chart data"}).click()
+
+    cy.addChartTitleAndLabels("iPhones vs. Androids", "iPhones", "Androids")
+
+    cy.addDataPoint("7", "2", 0)
+    cy.addDataPoint("5", "1", 1)
+    cy.addDataPoint("16", "0", 2)
+    cy.addDataPoint("4", "55", 3)
+    cy.addDataPoint("5", "40", 4)
+
+    cy.findByRole("button", { name: "Generate chart" }).click()
+    cy.findByRole("button", { name: "Save chart" }).click()
+
+    cy.findByText("Gallery").click()
+
+    cy.findByText("Cats vs. Dogs").should("exist")
+    cy.findByText("iPhones vs. Androids").should("exist")
+  })
 })
