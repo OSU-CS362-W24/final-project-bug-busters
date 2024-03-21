6f1efd67818525bf928ea0e2c457e23fcc39e662
Michael Blakeman <michael.l.blakeman@gmail.com>
2024-03-20 18:03:39 -0700

    Added UI integration tests for testing data entry for chart generation

--- src/line/line.test.js
+++ src/line/line.test.js
@@ -9,8 +9,6 @@ const userEvent = require("@testing-library/user-event").default
 
 const initDomFromFiles = require("../../test-utils/initDomFromFiles.js")
 
-
-
 describe("Integration tests for adding values in the chart builder...", () => {
   test("Testing functionality of add button in chart builder with multiple adds...", async () => {
     initDomFromFiles(
@@ -342,3 +340,163 @@ describe("Integration tests for clearing chart data", function() {
     expect(domTesting.getAllByRole(document, 'spinbutton').length).toBe(2)      //Check that the number of spinbuttons has been decreased back to the original size (2)
   })
 })
+
+describe("Integration tests for data sent to chart generation function", () => {
+  beforeEach(function() {
+    jest.resetModules()
+    jest.restoreAllMocks()
+  })
+  test("Two pairs of xy values", async function() {
+    jest.mock("../lib/generateChartImg.js")
+    const generateChartImgStub = require("../lib/generateChartImg")
+    generateChartImgStub.mockImplementation(function() {
+        return "http://placekitten.com/480/480"
+    })
+    
+	// I honestly have no clue why this comes after the mock, but it breaks if its not
+	initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`);
+	
+	const chartTitleInput = domTesting.getByLabelText(document.body, 'Chart title');
+	const xLabelInput = domTesting.getByLabelText(document.body, 'X label');
+	const yLabelInput = domTesting.getByLabelText(document.body, 'Y label');
+	const firstXValueInput = domTesting.getByTestId(document.body, 'x-0');
+	const firstYValueInput = domTesting.getByTestId(document.body, 'y-0');
+	const addButton = domTesting.getByText(document.body, '+');
+	const generateChartButton = domTesting.getByText(document.body, 'Generate chart');
+    const clearChartButton = domTesting.getByText(document, "Clear chart data")
+
+	const user = userEvent.setup();
+
+	await user.type(chartTitleInput, 'My Test Chart');
+	await user.type(xLabelInput, 'Time');
+	await user.type(yLabelInput, 'Value');
+	await user.type(firstXValueInput, '1');
+	await user.type(firstYValueInput, '100');
+
+	await user.click(addButton);
+	const secondXValueInput = domTesting.getByTestId(document.body, 'x-1');
+	const secondYValueInput = domTesting.getByTestId(document.body, 'y-1');
+	await user.type(secondXValueInput, '2');
+	await user.type(secondYValueInput, '200');
+
+	await user.click(generateChartButton);
+
+    expect(generateChartImgStub).toHaveBeenCalledTimes(1);
+	expect(generateChartImgStub).toHaveBeenCalledWith(
+  	  "line", // chart type 
+  	  expect.arrayContaining([
+    	expect.objectContaining({ x: "1", y: "100" }),
+    	expect.objectContaining({ x: "2", y: "200" })
+  	  ]),
+  	  "Time", 
+ 	  "Value",
+  	  "My Test Chart",
+  	  "#ff4500" // default color
+	);	
+    await user.click(clearChartButton)
+	generateChartImgStub.mockRestore()
+  });
+
+  test("One pair of XY values", async function() {
+    jest.mock("../lib/generateChartImg.js");
+    const generateChartImgStub = require("../lib/generateChartImg");
+    generateChartImgStub.mockImplementation(function() {
+        return "http://placekitten.com/480/480";
+    });
+
+    initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`);
+
+    const chartTitleInput = domTesting.getByLabelText(document.body, 'Chart title');
+    const xLabelInput = domTesting.getByLabelText(document.body, 'X label');
+    const yLabelInput = domTesting.getByLabelText(document.body, 'Y label');
+    const firstXValueInput = domTesting.getByTestId(document.body, 'x-0');
+    const firstYValueInput = domTesting.getByTestId(document.body, 'y-0');
+    const addButton = domTesting.getByText(document.body, '+');
+    const generateChartButton = domTesting.getByText(document.body, 'Generate chart');
+    const clearChartButton = domTesting.getByText(document, "Clear chart data")
+
+    const user = userEvent.setup();
+
+    await user.type(chartTitleInput, 'test');
+    await user.type(xLabelInput, 'Time');
+    await user.type(yLabelInput, 'Value');
+    await user.type(firstXValueInput, '1');
+    await user.type(firstYValueInput, '100');
+    await user.click(addButton);
+
+    // Setting custom color
+    await user.click(generateChartButton);
+    expect(generateChartImgStub).toHaveBeenCalledTimes(1);
+	expect(generateChartImgStub).toHaveBeenCalledWith(
+  	  "line", // chart type 
+  	  expect.arrayContaining([
+    	expect.objectContaining({ x: "1", y: "100" })
+  	  ]),
+  	  "Time", 
+ 	  "Value",
+  	  "test",
+  	  "#ff4500"
+	);	
+    await user.click(clearChartButton)
+	generateChartImgStub.mockRestore();
+  });
+
+  test("Three pairs of XY values", async function() {
+    jest.mock("../lib/generateChartImg.js");
+    const generateChartImgStub = require("../lib/generateChartImg");
+    generateChartImgStub.mockImplementation(function() {
+        return "http://placekitten.com/480/480";
+    });
+
+    initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`);
+
+    const chartTitleInput = domTesting.getByLabelText(document.body, 'Chart title');
+    const xLabelInput = domTesting.getByLabelText(document.body, 'X label');
+    const yLabelInput = domTesting.getByLabelText(document.body, 'Y label');
+    const firstXValueInput = domTesting.getByTestId(document.body, 'x-0');
+    const firstYValueInput = domTesting.getByTestId(document.body, 'y-0');
+    const addButton = domTesting.getByText(document.body, '+');
+    const generateChartButton = domTesting.getByText(document.body, 'Generate chart');
+    const clearChartButton = domTesting.getByText(document, "Clear chart data")
+
+    const user = userEvent.setup();
+
+    await user.type(chartTitleInput, 'Thirdtest');
+    await user.type(xLabelInput, 'Time');
+    await user.type(yLabelInput, 'Value');
+    await user.type(firstXValueInput, '1');
+    await user.type(firstYValueInput, '100');
+    await user.click(addButton);
+
+	const secondXValueInput = domTesting.getByTestId(document.body, 'x-1');
+	const secondYValueInput = domTesting.getByTestId(document.body, 'y-1');
+    await user.type(secondXValueInput, '2');
+    await user.type(secondYValueInput, '20');
+
+    await user.click(addButton);
+
+	const thirdXValueInput = domTesting.getByTestId(document.body, 'x-2');
+	const thirdYValueInput = domTesting.getByTestId(document.body, 'y-2');
+    await user.type(thirdXValueInput, '4');
+    await user.type(thirdYValueInput, '30');
+
+    await user.click(addButton);
+    
+	await user.click(generateChartButton);
+    expect(generateChartImgStub).toHaveBeenCalledTimes(1);
+	expect(generateChartImgStub).toHaveBeenCalledWith(
+  	  "line",
+  	  expect.arrayContaining([
+    	expect.objectContaining({ x: "1", y: "100" }),
+    	expect.objectContaining({ x: "2", y: "20" }),
+    	expect.objectContaining({ x: "4", y: "30" })
+  	  ]),
+  	  "Time", 
+ 	  "Value",
+  	  "Thirdtest",
+  	  "#ff4500"
+	);	
+    await user.click(clearChartButton)
+	generateChartImgStub.mockRestore();
+  });
+});
